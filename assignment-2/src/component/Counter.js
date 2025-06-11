ff1fa38900fd90e65944dd44e7663610d7549ec8
maheshgudadari <maheshgudadari@gmail.com>
2025-06-12 05:02:12 +0530

    added condition for number can not be less than 0

--- assignment-2/src/component/Counter.js
+++ assignment-2/src/component/Counter.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
-function Counter (props){
-  
-   let [count, setCount] = useState(Number(props.count));
-    function buttonClick(num){
-        setCount(prevCount => prevCount +(num));
-            }
- 
-return <>
-<h1>{count}</h1>
-<button onClick={()=>buttonClick(+1)}> Increment + </button>
-<button onClick={()=>buttonClick(-1)}> Decrement  - </button>
-</>
+function Counter(props) {
+
+    let [count, setCount] = useState(Number(props.count));
+    function buttonClick(num) {
+
+        if (count <= 0 && num === -1) {
+            setCount(prevCount => prevCount = 0);
+        } else {
+            setCount(prevCount => prevCount + (num));
+        }
+    }
+
+    return <>
+        <h1>{count}</h1>
+        <button onClick={() => buttonClick(+1)}> Increment + </button>
+        <button onClick={() => buttonClick(-1)}> Decrement  - </button>
+    </>
 }
 
 export default Counter;
